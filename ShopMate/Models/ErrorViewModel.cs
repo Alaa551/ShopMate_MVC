5715929e7c77ed841eeff6d446b1204a112e8d1d
Alaa Hassan <139705110+Alaa551@users.noreply.github.com>
2025-05-12 21:10:37 +0300

    Add project files.

---
+++ ShopMate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopMate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ShopMate/ViewModels/LoginVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopMate.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "*")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string? Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "*")]
+        public string? Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
